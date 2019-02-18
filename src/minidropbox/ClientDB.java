5c43b4affbdc4037f9489ee5385ee917811235d5
Absalom Herrera <45426790+elpatsan@users.noreply.github.com>
2019-02-18 12:41:39 -0600

    YA FUNCIONA SUBIR AL CIEN

--- src/minidropbox/ClientDB.java
+++ src/minidropbox/ClientDB.java
@@ -4,26 +4,25 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.net.Socket;
-import javax.swing.JFileChooser;
 
 /**
  *
- * @author elpat
+ * @author Absalom Herrera
  */
 public class ClientDB {
 
     private Socket s;
-    private String host = "127.0.0.1";
-    private int port = 1234;
-    private DataOutputStream dos;
-    private DataInputStream dis;
+    private String host;
+    private int port;
+    private DataOutputStream dosToServer;
+    private DataInputStream disFromFile;
 
     public ClientDB() {
         try {
-            s = new Socket(host, port);
+            host = "127.0.0.1";
+            port = 9999;
+            dosToServer = null;
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -31,46 +30,42 @@ public class ClientDB {
 
     public void upload(File[] filesToSend) {
         try {
-            dos = new DataOutputStream(s.getOutputStream());
             for (int i = 0; i < filesToSend.length; i++) {
                 send(filesToSend[i], "", filesToSend[i].getName());
             }
-            dos.writeChar(3);
-            dos.close();
-            s.close();
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void download (){
-        try{
-            dos = new DataOutputStream(s.getOutputStream());
-            recieveFiles ();
-        }catch (Exception e){
             e.printStackTrace();
         }
     }
 
     private void send(File file, String parentDirectory, String name) {
+        System.out.println("\nSENDING FILES/DIRECTORIES");
         try {
-            dos.writeChar(0);
-            dos.flush();
+            s = new Socket(host, port);
+            dosToServer = new DataOutputStream(s.getOutputStream());
             long size = file.length();
-            dos.writeLong(size);
-            dos.flush();
-            dos.writeUTF(name);
-            dos.flush();
-            dos.writeUTF(parentDirectory);
-            dos.flush();
+            char type;
             if (file.isDirectory()) {
-                dos.writeChar('d');
+                type = 'd';
+            } else {
+                type = 'f';
+            }
+            dosToServer.writeInt(1);
+            dosToServer.flush();
+            dosToServer.writeUTF(name);
+            dosToServer.flush();
+            dosToServer.writeUTF(parentDirectory);
+            dosToServer.flush();
+            dosToServer.writeChar(type);
+            dosToServer.flush();
+
+            if (type == 'd') {
                 File[] files = file.listFiles();
                 for (int i = 0; i < files.length; i++) {
                     File f = files[i];
                     size = f.length();
                     if (f.isDirectory()) {
-                        System.out.println("\nSENDING DIR: ");
+                        System.out.println("SENDING DIR: ");
                         if (parentDirectory.equals("")) {
                             System.out.println("PARENT: " + f.getParentFile().getName() + " NAME: " + name);
                             send(f, f.getParentFile().getName(), f.getName());
@@ -79,7 +74,7 @@ public class ClientDB {
                             send(f, parentDirectory + OsUtils.getSlash() + name, f.getName());
                         }
                     } else if (f.isFile()) {
-                        System.out.println("\nSENDING FILE RECURSIVE FROM INNER");
+                        System.out.println("SENDING FILE RECURSIVE FROM INNER");
                         if (parentDirectory.equals("")) {
                             System.out.println("EQEMPTY : PARENT: " + f.getParentFile().getName() + " NAME: " + f.getName());
                             send(f, f.getParentFile().getName(), f.getName());
@@ -90,96 +85,26 @@ public class ClientDB {
                     }
                 }
             } else {
-                System.out.println("\nACTUALLY SENDING FILE: NAME:" + name + " PARENT: " + parentDirectory);
-                dos.writeChar('f');
+                dosToServer.writeLong(size);
+                dosToServer.flush();
+                System.out.println("ACTUALLY SENDING FILE: NAME:" + name + " PARENT: " + parentDirectory);
                 long sent = 0;
                 int percent = 0, n = 0;
-                dis = new DataInputStream(new FileInputStream(file.getAbsolutePath()));
+                disFromFile = new DataInputStream(new FileInputStream(file.getAbsolutePath()));
                 while (sent < size) {
                     byte[] b = new byte[1500];
-                    n = dis.read(b);
-                    dos.write(b, 0, n);
-                    dos.flush();
+                    n = disFromFile.read(b);
+                    dosToServer.write(b, 0, n);
+                    dosToServer.flush();
                     sent += n;
                     percent = (int) ((sent * 100) / size);
                     System.out.print("\rSENT: " + percent + " %");
                 }
             }
+            dosToServer.close();
+            s.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    
-    private void recieveFiles() {
-        String clientRoute = "";
-        JFileChooser jfc = new JFileChooser();
-        jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        if (jfc.isMultiSelectionEnabled()) {
-            jfc.setMultiSelectionEnabled(false);
-        }
-        int l = jfc.showOpenDialog(null);
-
-        if (l == JFileChooser.APPROVE_OPTION) {
-            File f = jfc.getSelectedFile();
-            clientRoute = f.getAbsolutePath();
-        }
-        
-        if (!clientRoute.equals("")){
-            try {
-                dos.writeChar(1);
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        
-        try {
-            long size = dis.readLong();
-            String name = dis.readUTF();
-            String parent = dis.readUTF();
-            char option = dis.readChar();
-
-            if (option == 'd') {
-                System.out.println("\nCREATING DIR: " + "NAME: " + name + " PARENT: " + parent);
-                boolean bol = false;
-                if (parent.equals("")) {
-                    bol = new File(clientRoute + OsUtils.getSlash() + name).mkdir();
-                } else {
-                    bol = new File(clientRoute + OsUtils.getSlash() + parent + OsUtils.getSlash() + name).mkdir();
-                }
-                if (bol) {
-                    System.out.println("DIR CREATED: " + name);
-                } else {
-                    System.err.println("DIR COULDNT BE CREATED!");
-                }
-            } else {
-                System.out.println("\nCREATING FILE: " + "NAME: " + name + " PARENT: " + parent);
-
-                if (parent.equals("")) {
-                    dos = new DataOutputStream((new FileOutputStream(clientRoute + OsUtils.getSlash() + name)));
-                    System.out.println("DESTINATARY: " + clientRoute + OsUtils.getSlash() + name);
-                } else {
-                    dos = new DataOutputStream((new FileOutputStream(clientRoute + OsUtils.getSlash() + parent + OsUtils.getSlash() + name)));
-                    System.out.println("DESTINATARY: " + clientRoute + OsUtils.getSlash() + parent + OsUtils.getSlash() + name);
-                }
-
-                long r = 0;
-                int n = 0, percent = 0;
-                while (r < size) {
-                    byte[] b = new byte[1500];
-                    n = dis.read(b);
-                    dos.write(b, 0, n);
-                    dos.flush();
-                    r += n;
-                    percent = (int) ((r * 100) / size);
-                    System.out.print("\rRECEIVING: " + percent + "%");
-                }
-                dos.close();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 }
--- src/minidropbox/ServerDB.java
+++ src/minidropbox/ServerDB.java
@@ -3,7 +3,6 @@ package minidropbox;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,24 +10,23 @@ import javax.swing.JFileChooser;
 
 /**
  *
- * @author elpat
+ * @author Absalom Herrera
  */
 public class ServerDB {
- 
-    
 
-    // Route where files are going to be stored on the serer side
-    JFileChooser jfc = new JFileChooser();
-
-    
-    private static String serverRoute = "";
-
-    DataOutputStream dos;
-    DataInputStream dis;
-    FilePath p;
+    private ServerSocket s;
+    private Socket cl;
+    private JFileChooser jfc; // Route where files are going to be stored on the server side
+    private static String serverRoute;
+    private DataOutputStream dosToFile;
+    private DataInputStream disFromCl;
+    private FilePath paths; // if clients requeries to see whats stored on the server
+    private int clientRequest; // 0 = do nothing, 1 = upload
 
     public ServerDB() {
+        // Choosing server directory (for testing test)
         File f = null;
+        jfc = new JFileChooser();
         jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         if (jfc.isMultiSelectionEnabled()) {
             jfc.setMultiSelectionEnabled(false);
@@ -37,78 +35,66 @@ public class ServerDB {
         if (r == JFileChooser.APPROVE_OPTION) {
             f = jfc.getSelectedFile();
         }
-        serverRoute = f.getAbsolutePath();
-        
-        p = new FilePath();
-        ServerSocket s;
-        Socket cl;
 
-        // Client can upload (0), download (1), exit (3), show (4)
-        char clientRequest;
+        disFromCl = null;
+        serverRoute = f.getAbsolutePath();
+        paths = new FilePath();
+        cl = null;
+        clientRequest = 0;
+
         try {
-            s = new ServerSocket(1234);
+            s = new ServerSocket(9999);
             s.setReuseAddress(true);
-            System.out.println("SERVER ON, WAITING FOR CLIENT CONNECTION");
-            cl = s.accept();
-            if (cl != null) {
-                System.out.println("CONNECTED FROM: " + cl.getInetAddress() + " PORT: " + cl.getPort());
-            }
+        } catch (Exception e) {
+            System.err.println("CONSTRUCTOR ERROR:\n");
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
+
+        try {
             for (;;) {
-                if (!cl.isConnected()) {
-                    cl = s.accept();
-                    System.out.println("CONNECTED FROM: " + cl.getInetAddress() + " PORT: " + cl.getPort());
-                } else {
-                    dis = new DataInputStream(cl.getInputStream());
-                    clientRequest = dis.readChar();
-                    // Client requests to upload:
-                    if (clientRequest == 0) {
-                        recieveFiles();
-                    }
-                    else if (clientRequest == 3){
-                        for (int i = 0; i < p.getPaths().size(); i++) {
-                            String k = p.getPaths().get(i).getKey();
-                            String v = p.getPaths().get(i).getValue();
-                            System.out.println("path: " + k + " type: " + v);
-                        }
-                        
-                        cl.close();
-                        break;
-                    }
-                    else if (clientRequest == 1){
-                        
-                        //send();
-                    }
-                    else if (clientRequest == 4){
-                        
-                    }
+                System.out.println("\nSERVER ON, WAITING FOR CLIENT CONNECTION");
+                cl = s.accept();
+                System.out.println("CLIENT FROM: " + cl.getInetAddress() + " PORT: " + cl.getPort());
+                disFromCl = new DataInputStream(cl.getInputStream());
+                clientRequest = disFromCl.readInt();
+                if (clientRequest == 1) {
+                    recieveFiles();
                 }
             }
         } catch (Exception e) {
+            System.err.println("CONNECT ERROR:\n");
             e.printStackTrace();
         }
     }
 
     private void recieveFiles() {
-        try {
-            long size = dis.readLong();
-            String name = dis.readUTF();
-            String parent = dis.readUTF();
-            char option = dis.readChar();
+        System.out.println("\nRECEIVING FILES/DIRECTORIES");
+        long size;
+        String name, parentDirectory;
+        char type;
 
-            if (option == 'd') {
-                System.out.println("\nCREATING DIR: " + "NAME: " + name + " PARENT: " + parent);
+        try {
+            name = disFromCl.readUTF();
+            parentDirectory = disFromCl.readUTF();
+            type = disFromCl.readChar();
+
+            if (type == 'd') {
+                System.out.println("CREATING DIR: " + "NAME: " + name + " PARENT: " + parentDirectory);
                 boolean bol = false;
-                if (parent.equals("")) {
-                    
+                if (parentDirectory.equals("")) {
+
                     bol = new File(serverRoute + OsUtils.getSlash() + name).mkdir();
-                    
-                    p.insert(serverRoute + OsUtils.getSlash() + name, "d");
-                    
+
+                    paths.insert(serverRoute + OsUtils.getSlash() + name, "d");
+
                 } else {
-                    bol = new File(serverRoute + OsUtils.getSlash() + parent + OsUtils.getSlash() + name).mkdir();
-                    
-                    p.insert(serverRoute + OsUtils.getSlash() + name, "d");
-                    
+                    bol = new File(serverRoute + OsUtils.getSlash() + parentDirectory + OsUtils.getSlash() + name).mkdir();
+
+                    paths.insert(serverRoute + OsUtils.getSlash() + name, "d");
+
                 }
                 if (bol) {
                     System.out.println("DIR CREATED: " + name);
@@ -116,101 +102,46 @@ public class ServerDB {
                     System.err.println("DIR COULDNT BE CREATED!");
                 }
             } else {
-                System.out.println("\nCREATING FILE: " + "NAME: " + name + " PARENT: " + parent);
+                size = disFromCl.readLong();
+                System.out.println("CREATING FILE: " + "NAME: " + name + " PARENT: " + parentDirectory);
 
-                if (parent.equals("")) {
-                    dos = new DataOutputStream((new FileOutputStream(serverRoute + OsUtils.getSlash() + name)));
-                    
-                    p.insert(serverRoute + OsUtils.getSlash() + name, "f");
-                    
+                if (parentDirectory.equals("")) {
+                    dosToFile = new DataOutputStream((new FileOutputStream(serverRoute + OsUtils.getSlash() + name)));
+
+                    paths.insert(serverRoute + OsUtils.getSlash() + name, "f");
+
                     System.out.println("DESTINATARY: " + serverRoute + OsUtils.getSlash() + name);
                 } else {
-                    dos = new DataOutputStream((new FileOutputStream(serverRoute + OsUtils.getSlash() + parent + OsUtils.getSlash() + name)));
-                    
-                    p.insert(serverRoute + OsUtils.getSlash() + parent + OsUtils.getSlash() + name, "f");
-                    
-                    System.out.println("DESTINATARY: " + serverRoute + OsUtils.getSlash() + parent + OsUtils.getSlash() + name);
+                    dosToFile = new DataOutputStream((new FileOutputStream(serverRoute + OsUtils.getSlash() + parentDirectory + OsUtils.getSlash() + name)));
+
+                    paths.insert(serverRoute + OsUtils.getSlash() + parentDirectory + OsUtils.getSlash() + name, "f");
+
+                    System.out.println("DESTINATARY: " + serverRoute + OsUtils.getSlash() + parentDirectory + OsUtils.getSlash() + name);
                 }
 
                 long r = 0;
                 int n = 0, percent = 0;
                 while (r < size) {
                     byte[] b = new byte[1500];
-                    n = dis.read(b);
-                    dos.write(b, 0, n);
-                    dos.flush();
+                    n = disFromCl.read(b);
+                    dosToFile.write(b, 0, n);
+                    dosToFile.flush();
                     r += n;
                     percent = (int) ((r * 100) / size);
                     System.out.print("\rRECEIVING: " + percent + "%");
                 }
-                dos.close();
+                dosToFile.close();
             }
 
         } catch (Exception e) {
+            System.err.println("RECEIVE ERROR:\n");
             e.printStackTrace();
         }
 
-    }
-    
-    private void send(File file, String parentDirectory, String name) {
-        try {
-            dos.writeChar(0);
-            dos.flush();
-            long size = file.length();
-            dos.writeLong(size);
-            dos.flush();
-            dos.writeUTF(name);
-            dos.flush();
-            dos.writeUTF(parentDirectory);
-            dos.flush();
-            if (file.isDirectory()) {
-                dos.writeChar('d');
-                File[] files = file.listFiles();
-                for (int i = 0; i < files.length; i++) {
-                    File f = files[i];
-                    size = f.length();
-                    if (f.isDirectory()) {
-                        System.out.println("\nSENDING DIR: ");
-                        if (parentDirectory.equals("")) {
-                            System.out.println("PARENT: " + f.getParentFile().getName() + " NAME: " + name);
-                            send(f, f.getParentFile().getName(), f.getName());
-                        } else {
-                            System.out.println("PARENT: " + parentDirectory + " NAME: " + name);
-                            send(f, parentDirectory + OsUtils.getSlash() + name, f.getName());
-                        }
-                    } else if (f.isFile()) {
-                        System.out.println("\nSENDING FILE RECURSIVE FROM INNER");
-                        if (parentDirectory.equals("")) {
-                            System.out.println("EQEMPTY : PARENT: " + f.getParentFile().getName() + " NAME: " + f.getName());
-                            send(f, f.getParentFile().getName(), f.getName());
-                        } else {
-                            System.out.println("PARENT: " + parentDirectory + OsUtils.getSlash() + name + " NAME: " + f.getName());
-                            send(f, parentDirectory + OsUtils.getSlash() + name, f.getName());
-                        }
-                    }
-                }
-            } else {
-                System.out.println("\nACTUALLY SENDING FILE: NAME:" + name + " PARENT: " + parentDirectory);
-                dos.writeChar('f');
-                long sent = 0;
-                int percent = 0, n = 0;
-                dis = new DataInputStream(new FileInputStream(file.getAbsolutePath()));
-                while (sent < size) {
-                    byte[] b = new byte[1500];
-                    n = dis.read(b);
-                    dos.write(b, 0, n);
-                    dos.flush();
-                    sent += n;
-                    percent = (int) ((sent * 100) / size);
-                    System.out.print("\rSENT: " + percent + " %");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {
         ServerDB sdb = new ServerDB();
+        sdb.connect();
     }
 }
